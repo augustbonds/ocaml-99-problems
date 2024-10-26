ded9f1a774906d2a26955e367280762ae8d7f90a
August Bonds <hello@augustbonds.com>
2024-10-26 10:11:33 +0200

    Add palindrome check and move functions to lib/

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,7 @@
+open Problems_99.Functions
+
 let () = print_newline ()
 let () = print_endline "Hello, World!"
-
-let rec last l =
-  match l with
-    | [] -> None
-    | h :: [] -> Some h
-    | _ :: t -> last t
 
 let () = 
   let elem = last ["a"; "b"; "c"] in
@@ -13,26 +9,11 @@ let () =
   | Some elem -> (print_endline elem)
   | None -> ()
 
-
-let rec last l =
-  match l with
-    | [] -> None
-    | [x] -> Some x
-    | _ :: tail -> last tail
-
 let () = 
   let elem = last ["a"; "b"; "c"; "d"] in
   match elem with
   | Some elem -> (print_endline elem)
   | None -> ()
-
-
-let rec last_two l =
-  match l with
-    | [] -> None
-    | _ :: [] -> None
-    | h :: h' :: [] -> Some (h, h')
-    | _ :: h' :: t -> last_two (h' :: t)  (* could just use "t" here...  *)
 
 let () =
   let elem = last_two ["a"; "b"; "c"] in
@@ -46,13 +27,11 @@ let () =
     | Some (x, y) -> Printf.printf "%s%s\n" x y
     | None -> ()
 
-
-let rec nth n l =
-  match l with
-    | [] -> None
-    | h :: t -> match n with
-      | 0 -> Some h
-      | n -> nth (n-1) t
+let () =
+  let elem = nth 4 ["a"; "b"; "c"; "d"; "e"; "f"] in
+  match elem with
+    | Some x -> Printf.printf "%s\n" x
+    | None -> ()
 
 let () =
   let elem = nth 4 ["a"; "b"; "c"; "d"; "e"; "f"] in
@@ -60,49 +39,30 @@ let () =
     | Some x -> Printf.printf "%s\n" x
     | None -> ()
 
-let rec nth idx list =
-  match (idx, list) with
-  | (_, []) -> None
-  | (0, h :: _) -> Some h
-  | (i, _ :: t) -> nth (i-1) t
-
-let () =
-  let elem = nth 4 ["a"; "b"; "c"; "d"; "e"; "f"] in
-  match elem with
-    | Some x -> Printf.printf "%s\n" x
-    | None -> ()
-
-let rec len l =
-  match l with
-   | [] -> 0
-   | _ :: t -> 1 + len t
-
 let () =
   let length = len ["a"; "b"; "c"; "d"; "e"; "f"] in
   Printf.printf "length is :%d\n" length
 
-let len l =
-  let rec aux acc l =
-    match l with
-      | [] -> acc
-      | _ :: t -> aux (acc+1) t
-  in
-  aux 0 l
-
 let () =
   let length = len ["a"; "b"; "c"; "d"; "e"; "f"] in
   Printf.printf "length is :%d\n" length
-
-let reverse l =
-  let rec aux acc l =
-    match l with
-      | [] -> acc
-      | h :: t -> aux (h :: acc) t
-  in
-  aux [] l
-
 
 let () = 
   match reverse ["a"; "b"; "c"] |> nth 0 with 
   | None -> ()
   | Some x -> Printf.printf "first in reverse: %s\n" x
+
+let () = 
+  match is_palindrome ["a"; "b"; "a"] with 
+  | true -> Printf.printf "it's a palindrome!\n"
+  | false -> Printf.printf "it's not a palindrome..\n"
+
+let () = 
+  match is_palindrome_str_opt "aba" with 
+  | true -> Printf.printf "it's a palindrome!\n"
+  | false -> Printf.printf "it's not a palindrome..\n"
+
+let () = 
+  match is_palindrome_opt (Array.of_list ["a"; "b"; "a"]) with 
+  | true -> Printf.printf "it's a palindrome!\n"
+  | false -> Printf.printf "it's not a palindrome..\n"
---
+++ lib/functions.ml
@@ -0,0 +1,86 @@
+let rec last l =
+  match l with
+    | [] -> None
+    | h :: [] -> Some h
+    | _ :: t -> last t
+
+let rec last l =
+  match l with
+    | [] -> None
+    | [x] -> Some x
+    | _ :: tail -> last tail
+
+let rec last_two l =
+  match l with
+    | [] -> None
+    | _ :: [] -> None
+    | h :: h' :: [] -> Some (h, h')
+    | _ :: h' :: t -> last_two (h' :: t)  (* could just use "t" here...  *)
+
+
+let rec nth n l =
+  match l with
+    | [] -> None
+    | h :: t -> match n with
+      | 0 -> Some h
+      | n -> nth (n-1) t
+
+let rec nth idx list =
+  match (idx, list) with
+  | (_, []) -> None
+  | (0, h :: _) -> Some h
+  | (i, _ :: t) -> nth (i-1) t
+
+let rec len l =
+  match l with
+   | [] -> 0
+   | _ :: t -> 1 + len t
+
+let len l =
+  let rec aux acc l =
+    match l with
+      | [] -> acc
+      | _ :: t -> aux (acc+1) t
+  in
+  aux 0 l
+
+let reverse l =
+  let rec aux acc l =
+    match l with
+      | [] -> acc
+      | h :: t -> aux (h :: acc) t
+  in
+  aux [] l
+
+let rec same l1 l2 = 
+  match (l1, l2) with
+    | ([], []) -> true 
+    | (h1 :: x1 , h2 :: x2) -> if h1 = h2 then same x1 x2 else false
+    | _ -> false
+  
+
+let is_palindrome l = 
+  let rev = reverse l in
+  same l rev
+
+let is_palindrome l = 
+  l = reverse l
+
+
+let is_palindrome_str_opt s =
+  let len = String.length s in
+  let rec check i =
+    if i >= 2 then true
+    else if s.[i] <> s.[len-i-1] then false
+    else check (i+1)
+  in
+  check 0
+
+let is_palindrome_opt l = 
+  let len = Array.length l in
+  let rec check i = 
+    if i >= 2 then true
+    else if Array.get l i <> Array.get l (len-i-1) then false
+    else check (i+1)
+  in
+  check 0
